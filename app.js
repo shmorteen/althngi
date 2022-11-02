6601bf2bf2e4275fe4aa470b71dfb5c275db299a
shmorteen <shmorteen@gmail>
2022-11-02 02:59:57 +0000

    FIRST COMMIT

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+
+
+
+
+
+let user = [
+    {
+        slackUsername:'shmorteen',
+        backend: 'yes',
+        age: 26,
+        bio:'Tech enthusiast hoping to gain more experience'
+    }
+];
+
+app.get('/', (req, res) => {
+    res.send({message:'LOOKING GOOD'})
+});
+
+app.get('/user',(req, res) => {
+    res.json(user)
+});
+
+app.listen(PORT, () => console.log("App server is currently running on port " + PORT));
